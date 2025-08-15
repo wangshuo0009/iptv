2b30871191c35b6073cc67f06c25d044061bc46d
wangshuo0009 <wangshuo0009@gmail.com>
2025-08-15 10:09:13 +0800

    更新配置

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 export default {
     async fetch(request, env, ctx) {
-        const url = new URL(request.url);
-
-        if (url.pathname.endsWith(".m3u") && !/^\/iptv\/([^/]+)\.m3u$/.test(url.pathname)) {
-            url.pathname = "/iptv" + url.pathname;
-            request = new Request(url.toString(), request);
-        }
+        // const url = new URL(request.url);
+        //
+        // if (url.pathname.endsWith(".m3u") && !/^\/iptv\/([^/]+)\.m3u$/.test(url.pathname)) {
+        //     url.pathname = "/iptv" + url.pathname;
+        //     request = new Request(url.toString(), request);
+        // }
 
         return fetch(request);
     }
