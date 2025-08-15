e695e3f92a94881ff5a9e64885690d019cd6f193
wangshuo0009 <wangshuo0009@gmail.com>
2025-08-15 09:34:36 +0800

    更新配置

--- worker.js
+++ worker.js
@@ -1,12 +1,13 @@
 export default {
     async fetch(request, env, ctx) {
         const url = new URL(request.url);
+        url.href = 'https://iptv.wangshuos.com/jslt.m3u'
         let pathname = url.pathname;
-
-        // 如果不是 /iptv/ 开头，并且以 .m3u 结尾
-        if (pathname.endsWith('.m3u') && !url.href.match(/^\/iptv\/([^/]+)\.m3u$/)) {
-            request.href = url.origin + "/iptv" + url.pathname;
+        if (pathname.endsWith(".m3u") && !/^\/iptv\/([^/]+)\.m3u$/.test(url.pathname)) {
+            url.pathname = "/iptv" + url.pathname;
+            request = new Request(url.toString(), request);
         }
+
         return fetch(request);
     }
 };
