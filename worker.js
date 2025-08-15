1cb42090c02aedceb9eb8aec5b6c0016caacc4ad
wangshuo0009 <wangshuo0009@gmail.com>
2025-08-15 09:06:54 +0800

    更新配置

--- worker.js
+++ worker.js
@@ -4,27 +4,9 @@ export default {
         let pathname = url.pathname;
 
         // 如果不是 /iptv/ 开头，并且以 .m3u 结尾
-        if (pathname.endsWith('.m3u') && !pathname.startsWith('/iptv/')) {
-            const lastSlash = pathname.lastIndexOf('/');
-            const dotM3u = pathname.lastIndexOf('.m3u');
-            const fileName = pathname.substring(lastSlash + 1, dotM3u);
-
-            // 改写路径到 /iptv/ 目录
-            pathname = '/iptv/'+ fileName +'.m3u';
+        if (pathname.endsWith('.m3u') && !url.href.match(/^\/iptv\/([^/]+)\.m3u$/)) {
+            request.href = url.origin + "/iptv" + url.pathname;
         }
-
-        // 用请求的 host 拼成完整 URL
-        // const targetUrl = '${url.protocol}//${url.host}${pathname}';
-        const targetUrl = 'https://iptv.wangshuos.com' + pathname
-        
-        // 发请求并返回，同时设置正确 Content-Type
-        const response = await fetch(targetUrl);
-        return new Response(response.body, {
-            status: response.status,
-            headers: {
-                ...response.headers,
-                'Content-Type': 'audio/x-mpegurl; charset=utf-8'
-            }
-        });
+        return fetch(request);
     }
 };
