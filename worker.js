40218ec5b8802b972ae27dcef998305033145b99
wangshuo0009 <wangshuo0009@gmail.com>
2025-08-15 09:43:10 +0800

    更新配置

--- worker.js
+++ worker.js
@@ -1,9 +1,8 @@
 export default {
     async fetch(request, env, ctx) {
         const url = new URL(request.url);
-        url.href = 'https://iptv.wangshuos.com/jslt.m3u'
-        let pathname = url.pathname;
-        if (pathname.endsWith(".m3u") && !/^\/iptv\/([^/]+)\.m3u$/.test(url.pathname)) {
+
+        if (url.pathname.endsWith(".m3u") && !/^\/iptv\/([^/]+)\.m3u$/.test(url.pathname)) {
             url.pathname = "/iptv" + url.pathname;
             request = new Request(url.toString(), request);
         }
