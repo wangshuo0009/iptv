0154c2cf4a9b4a2deb1549b353a918f28ba64250
wangshuo0009 <wangshuo0009@gmail.com>
2025-08-15 10:25:49 +0800

    更新配置

--- worker.js
+++ worker.js
@@ -1,12 +1,18 @@
 export default {
     async fetch(request, env, ctx) {
-        // const url = new URL(request.url);
-        //
-        // if (url.pathname.endsWith(".m3u") && !/^\/iptv\/([^/]+)\.m3u$/.test(url.pathname)) {
-        //     url.pathname = "/iptv" + url.pathname;
-        //     request = new Request(url.toString(), request);
-        // }
+        var endWith = request.url.pathname.endsWith(".m3u");
+        var isFullM3uPath = /^\/iptv\/([^/]+)\.m3u$/.test(request.url.pathname);
+        if (endWith && !isFullM3uPath) {
+            request.url.pathname = "/iptv" + request.url.pathname;
+            return fetch(request);
 
-        return fetch(request);
+        } else if (request.url.pathname !== '/' && !endWith) {
+            var response = new Response();
+            response.status = 404
+            return response;
+        } else {
+            return fetch(request);
+        }
+
     }
 };
